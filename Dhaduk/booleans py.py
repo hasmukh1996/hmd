1fd3bdc0b64499c49ce95a99d0eb0e8222f9e5a7
hasmukh1996 <hasmukhkumar.dhaduk@stud.th-deg.de>
2022-05-24 03:53:52 +0200

    rrfg

---
+++ Dhaduk/booleans py.py
@@ -0,0 +1,29 @@
+x= 10
+y= 10
+print(x>y)
+print(x<y)
+print(x == y)
+
+a = 1055
+b= 105
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+def myFunction():
+    return False
+
+print(myFunction())
+
+def Hasmukh():
+    return True
+
+if Hasmukh():
+    print("Yes")
+else:
+    print("No")
+
+    
+
+
